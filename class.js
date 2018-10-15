6301e89ae333ff735b11331ba03879c66bc705d1
ey <eyy0221@gmail.com>
2018-10-15 19:42:54 -0400

    renaming week2

--- class.js
+++ class.js
@@ -1,27 +1,3 @@
-// function add(){
-//   var sum = 0
-//   for(var i= 0; i < arguments.length; i++ ){
-//     sum += arguments[i];
-//   }
-//   return sum;
-// }
-//
-// const addTotal = add(1,2,3,4,5,6,7,8,9);
-
-// var billAmount = Math.random() * 50;
-//
-// function gratuity(amount){
-//   return amount * 0.2;
-// };
-//
-// function totalWithGrat(amount){
-//   return amount + gratuity(amount);
-// };
-//
-//
-//
-// console.log('Your total is ' + totalWithGrat(billAmount).toFixed(2));
-
 // var add = function(a,b){
 //   return a+b;
 // }

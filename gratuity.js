1ce71c6e84e27953ab9a85262d1e3098d310eb24
eewhy <40720321+eewhy@users.noreply.github.com>
2018-10-25 13:13:09 -0400

    Update gratuity.js

--- gratuity.js
+++ gratuity.js
@@ -1,13 +1,3 @@
-function add(){
-  var sum = 0
-  for(var i= 0; i < arguments.length; i++ ){
-    sum += arguments[i];
-  }
-  return sum;
-}
-
-const addTotal = add(1,2,3,4,5,6,7,8,9);
-
 var billAmount = Math.random() * 50;
 
 function gratuity(amount){
